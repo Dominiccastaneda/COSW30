ce44f6ab2a1e182b3f986d70a1b7dcf8122d8052
EC2 Default User <ec2-user@ip-172-31-89-211.ec2.internal>
2019-08-29 02:54:52 +0000

    created lab 1 file

---
+++ Labwork/LAB1.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html>
+    
+    <head>
+        <title>Lab 1 PHP page</title>
+    </head>
+    <body>
+        <h1>name</h1>
+        <p>"Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur?"
+        </p>
+
+        <?php
+            echo 'This is PHP text';
+            echo '<a href=""> this is my PHP paragraph ';
+
+            
+        ?>    
+        
+    </body>
+    
+    
+    
+</html>
+
+
+<?php
+    echo 'this is a string';
+    echo '<h1>this is a header</h1>'
+    echo '<p1>this is a paragraph</p>'
+
+?>
