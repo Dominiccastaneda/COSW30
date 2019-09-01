e822a9d1863d9fcb76c11da8032c1c436a42931b
Dominiccastaneda <Dominiccastaneda@gmail.com>
2019-09-01 22:51:48 +0000

    Lab 1 ver 1.1

--- Labwork/lab1.php
+++ Labwork/lab1.php
@@ -1,30 +1,30 @@
 <!doctype html>
 <html>
     <style type="text/css">
-        title {color: red;
-            font-size: 400%;
-        }
-        h1 {color: #64c365;}
-        p {border: 2px solid black;}
+        h1 {color: #64c365;
+            font-size:400%;
+            }
+        p {border: 2px solid blue;}
+    
     </style>
+    
     <head>
         <title>Labwork 1: Hello World</title>
     </head>
     <body>
         <h1>Dominic Castaneda</h1>
-        <p>My name is Dominic Castaneda.  I am 29 years old and I am in the database management program at LBCC.  My hobbies include reading and video games.  
+        <p>My name is Dominic Castaneda.  I am 29 years old and I am in the database management program at LBCC, returning to being a fulltime student after working as a data analyst at Molina Healthcare in Long Beach.  My hobbies include reading and video games.  
+        I own a Nintendo switch and play many PC games.
         My github profile is <a href="https://github.com/Dominiccastaneda">here</a>.
         
         </p>
 
         <?php
-            echo 'I found this assignment to be a refreshing review of html and basic php, though some of the intricacies of git were hard to parse at first. Two of my favorite websites which used for refrence when coding this page are <a href="https://www.w3schools.com">W3Schools</a> and <a href="https://stackoverflow.com/">Stack Overflow</a>.  ';
-            
-
-            
-        ?>    
+            echo "<p> I found this assignment to be a refreshing review of html and basic php, though some of the intricacies of git were hard to parse at first. Two of my favorite websites which used for reference when coding this page are <a href='https://www.w3schools.com'>W3Schools</a> and <a href='https://stackoverflow.com/'>Stack Overflow</a>. <br> 
+            I also used <a href='http://git.huit.harvard.edu/guide/'>this</a> git reference page.</p>";
         
-    </body>
+        ?>
 
     
+    </body>
 </html>
