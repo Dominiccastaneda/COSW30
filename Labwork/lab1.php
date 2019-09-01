32672e47d9694d9c43a2f65eb079411eaf6f0f46
Dominiccastaneda <Dominiccastaneda@gmail.com>
2019-09-01 22:08:51 +0000

    Lab 1 ver 1

---
+++ Labwork/lab1.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html>
+    <style type="text/css">
+        title {color: red;
+            font-size: 400%;
+        }
+        h1 {color: #64c365;}
+        p {border: 2px solid black;}
+    </style>
+    <head>
+        <title>Labwork 1: Hello World</title>
+    </head>
+    <body>
+        <h1>Dominic Castaneda</h1>
+        <p>My name is Dominic Castaneda.  I am 29 years old and I am in the database management program at LBCC.  My hobbies include reading and video games.  
+        My github profile is <a href="https://github.com/Dominiccastaneda">here</a>.
+        
+        </p>
+
+        <?php
+            echo 'I found this assignment to be a refreshing review of html and basic php, though some of the intricacies of git were hard to parse at first. Two of my favorite websites which used for refrence when coding this page are <a href="https://www.w3schools.com">W3Schools</a> and <a href="https://stackoverflow.com/">Stack Overflow</a>.  ';
+            
+
+            
+        ?>    
+        
+    </body>
+
+    
+</html>
