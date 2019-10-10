8d389f83336b3a2df668bcbe138f3caf480b3e16
Dominiccastaneda <dominiccastaneda@gmail.com>
2019-10-10 01:33:47 +0000

    ftest

--- testing/function.php
+++ testing/function.php
@@ -1,10 +1,17 @@
 <?php
-$name= ['aa', 'bb', 'cc'];
+$names= ['aa', 'bb', 'cc'];
 
 global $nametest = 'mario';
 
 funciton greeting ($nametest){
     echo '<p>hello, my name is' .$nametest. '</p>';
+
+}
+
+
+foreach  $names as $name() {
+    greeting($name);
+
 
 }
 
